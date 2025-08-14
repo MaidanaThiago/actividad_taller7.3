a31cecc47fa70d300f68aa2aece2389f1d48aeac
MaidanaThiago <maidana15t@gmail.com>
2025-08-14 18:18:51 -0300

    first commit

---
+++ registro/registro/js/validacion.js
@@ -0,0 +1,8 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
