98b648e8507e5710442bda0f96f05b3d19074f96
MaidanaThiago <maidana15t@gmail.com>
2025-08-14 18:35:44 -0300

    Funcion de validar contraseña finalizada

--- js/validacion.js
+++ js/validacion.js
@@ -6,3 +6,16 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
+
+
+function validarPassword(){
+    const contraseña = document.getElementById("password1").value;
+    const repitecontraseña = document.getElementById("password2").value;
+
+    if(contraseña === repitecontraseña){
+        return true;
+    }else{
+        return false;
+    }
+}
+
