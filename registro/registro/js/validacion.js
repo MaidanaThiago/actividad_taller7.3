ee3367d3e057493005a5b2087b7a0bcf574f1601
juchhd <mccomepiza-@gmail.com>
2025-08-14 18:37:12 -0300

    minimo de caracteres

--- registro/registro/js/validacion.js
+++ registro/registro/js/validacion.js
@@ -1,3 +1,12 @@
+document.getElementById("regBtn").addEventListener("click", function() {
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    if(password1.length < 6 || password2.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres.");
+        return;
+    }
+});
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
