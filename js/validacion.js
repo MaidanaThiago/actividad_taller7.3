5ac772f447e0f84ce58aa6804b005de0313cf9ad
RobertoRodriguezR <roberodri68@gmail.com>
2025-08-14 18:46:58 -0300

    Código para que salte el error si hay campos vacíos

--- js/validacion.js
+++ js/validacion.js
@@ -6,3 +6,22 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
+let elBoton = document.getElementById("regBtn")
+let elNombre = document.getElementById("nombre");
+let elApellido = document.getElementById("apellido");
+let elEmail = document.getElementById("email");
+let elPassword = document.getElementById("password1");
+let elPasswordRepetido = document.getElementById("password2");
+
+function sinCamposVacios(){
+    if (elNombre.value === "" || elApellido.value === "" || elEmail.value === "" || elPassword.value === "" || elPasswordRepetido.value === ""){
+        console.log("¡Hay campos vacíos en el formulario!");
+        showAlertError();
+    } else {
+        console.log("¡Formulario completo!");
+    }
+}
+
+elBoton.addEventListener("click", sinCamposVacios);
+
+
